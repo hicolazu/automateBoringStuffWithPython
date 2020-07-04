f832dd9365c52169844d714ac729d1e1b80d8d88
Henrico <hicolazu22@hotmail.com>
2020-07-04 13:46:28 -0300

    INITIAL COMMIT

---
+++ basic/flowControll/ifElse.py
@@ -0,0 +1,8 @@
+print('Hello, enter your name: ')
+name = input()
+if name == 'Henrico':
+    print('Hi, Henrico')
+elif name == 'User':
+    print('Hi, User')
+else:
+    print('Hi!')
---
+++ basic/flowControll/while.py
@@ -0,0 +1,6 @@
+spam = 0
+while spam < 5:
+    print("Value: " + str((spam + 1)))
+    if spam == 3:
+        break
+    spam += 1
